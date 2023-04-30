288a10a3afdfb9fc937ab14f5f7f88871304f5ed
Aya <ayafarid700@gmail.com>
2023-04-30 16:52:04 +0300

    task 1 cat Week 3 DIO driver

---
+++ Week 3/DIO/Release/Debug/DIO_program.d
@@ -0,0 +1,10 @@
+DIO_program.o DIO_program.o: ../DIO_program.c ../DIO_errorstae.h \
+  ../STD_types.h ../DIO_private.h ../DIO_config.h
+
+../DIO_errorstae.h:
+
+../STD_types.h:
+
+../DIO_private.h:
+
+../DIO_config.h:
---
+++ Week 3/DIO/Release/Debug/main.d
@@ -0,0 +1,10 @@
+main.o main.o: ../main.c ../DIO_interface.h ../DIO_errorstae.h \
+  ../STD_types.h ../DIO_config.h
+
+../DIO_interface.h:
+
+../DIO_errorstae.h:
+
+../STD_types.h:
+
+../DIO_config.h:
