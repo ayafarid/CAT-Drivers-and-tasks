52e5b2176a85e251e76f82cd232ae3ff48f5bc5b
Aya <ayafarid700@gmail.com>
2023-07-21 11:33:24 +0300

    calculator task

--- Week 5/Calculator_Driver/HAL/LCD/LCD_int.h
+++ Week 5/Calculator_Driver/HAL/LCD/LCD_int.h
@@ -8,8 +8,10 @@
 #ifndef HAL_LCD_LCD_INIT_H_
 #define HAL_LCD_LCD_INIT_H_
 
+#define LCD_CLEAR    0x01
 ES_t LCD_enuInit(void);
 ES_t LCD_enuDisplayChar(u8 Copy_u8Data);
 ES_t LCD_enuSendCommand(u8 Copy_u8Commad);
-
+void LCD_Clear(void);
+void LCD_Line_Column(unsigned char line , unsigned char column);
 #endif /* HAL_LCD_LCD_INIT_H_ */
